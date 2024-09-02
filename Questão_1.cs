352737e2d79879a637dce8676ba3d9921b117a03
Igor <165734789+IgorMarchioni@users.noreply.github.com>
2024-09-02 19:35:54 -0300

    Questão_1.cs

--- Questão_1.cs
+++ Questão_1.cs
@@ -1,10 +1,4 @@
 using System;
-/*
-1. Faça um programa que receba duas notas, calcule e mostre a média ponderada dessas notas,
-considerando peso 2 para a primeira nota e peso 3 para a segunda nota
-2. Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-
-se que este sofreu um desconto de 10%.
-*/
 class Program {
   public static void Main (string[] args) {
   int a , b;
